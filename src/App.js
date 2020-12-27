a3a186643adb00d10b02cc620e03e44d104ec810
Joan Romero <romerojoan1999@gmail.com>
2020-12-27 11:32:52 -0400

    Add components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
-import { Navigation } from './components/Navigation/Navigation';
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
+
+const particlesOptions = {
+	particles: {
+		number: {
+			value: 10,
+			density: {
+				enable: true,
+				value_area: 100,
+			},
+		},
+	},
+};
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				<Particles params={particlesOptions} className="particles" />
 				<Navigation />
+				<Logo />
+				<Rank />
+				<ImageLinkForm />
 			</div>
 		);
 	}
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Rank = () => {
+	return (
+		<div>
+			<p className="white f3">{'Joan, your current rank is'}</p>
+			<p className="white f2">{'#5'}</p>
+		</div>
+	);
+};
+
+export default Rank;
